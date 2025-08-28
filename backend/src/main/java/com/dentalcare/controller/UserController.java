416c94b3badf947c0ac44f856c07b90ca1a80cd1
kadali <kadali1229@gmail.com>
2025-08-28 20:21:55 +0530

    user

--- backend/src/main/java/com/dentalcare/controller/UserController.java
+++ backend/src/main/java/com/dentalcare/controller/UserController.java
@@ -7,6 +7,7 @@ import com.dentalcare.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
